5e3d674c21fa297e08807c945e067c56b8809242
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2019-12-19 10:52:35 +0500

    Broke homepage component into smaller components and also added some styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import Directory from '../../directory/directory.component';
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
